59ca226e4602de1c1214dc86d97a8200f45b4e33
drenimorina12 <morinadreni85@gmail.com>
2024-11-21 18:45:21 -0800

    test

---
+++ kalkulatori.js
@@ -0,0 +1,22 @@
+let numri1 = +prompt("shkruaj numrin e pare");
+let operatori = prompt("shkruaj operatorin");
+let numri2 = +prompt("shkruaj numrin e dyte");
+
+
+console.log(numri1);
+console.log(operatori   );
+console.log(numri2);
+
+
+if (operatori == '+' ) {
+    alert(numri1 + numri2)
+}
+if (operatori == '*' ) {
+    alert(numri1 * numri2)
+}
+if (operatori == '/' ) {
+    alert(numri1 / numri2)
+}
+if (operatori == '-' ) {
+    alert(numri1 - numri2)
+}
---
+++ script.js
@@ -0,0 +1,23 @@
+// // operatoret unar
+// let numri1 = -4 ;
+// //console.log(numri1);
+// //e shendrron vleren ne number
+
+// let numri2 = +"4";
+// let numri3 = +false;
+
+// console.log("rreshti nr 9", ++numri1)
+// console.log("rreshti nr 10", numri2++)
+// console.log("rreshti nr 11", numri3)
+
+// // dekrement => zbritje
+
+// console.log("rreshti nr 9", --numri1)
+// console.log("rreshti nr 10", numri2--)
+// console.log("rreshti nr 11", numri3)
+
+let bora = confirm("a po bjen bore");
+
+if (bora == true) {
+    alert("Kron rri nshpi")
+}
